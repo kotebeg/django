9eb3e5a6d4833cbedc9ccc8b4b23c9869b500b30
kotebeg <kotebeg@gmail.com>
2024-10-28 07:20:15 +0400

    add readme file

--- dj_project_selectize/dj_project_selectize/settings.py
+++ dj_project_selectize/dj_project_selectize/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=^!o=65i-pi20m=y3=!ku1b9%u+j(_5ckv70p%phek3v#e4i_x'
+SECRET_KEY = 'django-insecure-7+u)y$g%b)$d2cv7*jyd7w9pd23a=q+u97m-vble+f&9m*l($m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
