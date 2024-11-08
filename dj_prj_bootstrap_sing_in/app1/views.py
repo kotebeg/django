654842e8febd8bcc2352f1cda647ea1b9a294141
kotebeg <kotebeg@gmail.com>
2024-11-08 16:16:29 +0400

    adding new features

--- dj_prj_bootstrap_sing_in/app1/views.py
+++ dj_prj_bootstrap_sing_in/app1/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+
+from django.contrib.auth import authenticate, login, logout
+
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
@@ -14,7 +21,17 @@ def index(request):
                 return render(request, 'app1/index.html')
 
 
+def home(request):
+        if request.method == "POST":
+                # print('--> POST')
+                # print('-->', request.POST, type(request.POST))
+                usr_nm = request.POST['floatingInput_name']
+                usr_ps = request.POST['floatingPassword_name']
+                # print('-->', request.POST['floatingInput_name'])
+                # print('-->', request.POST['floatingPassword_name'])
 
-def def_two(request):
-        pass
-
+                # user = authenticate(request, username = usr_nm, password = usr_ps)
+                return render(request, 'app1/home.html')
+        else:
+                print('-->', request.method)
+                return render(request, 'app1/home.html')
